b71753642ce37e74ad0da60718bd6bc51e4754e9
Sumit Maurya <mauryasumit222@gmail.com>
2026-05-19 12:38:13 +0530

    Refactor ApplicationController to use Doorkeeper for user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
-class ApplicationController < ActionController::API
-    before_action :authenticate_user!
+module Api
+  class ApplicationController < ActionController::API
+    before_action :doorkeeper_authorize!
+
+    private
+
+    # helper method to access the current user from the token
+    def current_user
+      @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
+    end
+  end
 end
